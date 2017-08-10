a62602c0bd603ff389d23613c689d7685b9a19e8
Oleksii Volkov <lifecoder@MacBook-Pro-MacPro.local>
2017-08-10 22:39:32 +0300

    some intro

---
+++ ui/src/components/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <h2>{{ msg }}</h2>
+</template>
+
+<script>
+export default {
+  name: 'RetroX',
+  data: () => {
+    return {
+      msg: 'wut'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
